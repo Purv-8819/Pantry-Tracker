e79ef4466e4c9d2c96fec4cce94522fdecb398f0
Purv-8819 <purvpatel905@gmail.com>
2024-07-30 16:33:55 -0500

    Completed Sibling App

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,22 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { collection, addDoc, getFirestore } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDoc,
+  querySnapshot,
+  query,
+  onSnapshot,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 import { db } from "./firebase";
 
 export default function Home() {
   const [items, setItems] = useState([
-    { name: "Coffee", price: 4.95 },
-    { name: "Movie", price: 24.95 },
-    { name: "candy", price: 7.95 },
+    // { name: "Coffee", price: 4.95 },
+    // { name: "Movie", price: 24.95 },
+    // { name: "candy", price: 7.95 },
   ]);
 
   const [newItem, setNewItem] = useState({ name: "", price: "" });
@@ -23,12 +32,38 @@ export default function Home() {
         name: newItem.name.trim(),
         price: newItem.price,
       });
+      setNewItem({ name: "", price: "" });
     }
   };
 
   //Read Items from database
+  useEffect(() => {
+    const q = query(collection(db, "items"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let itemsArr = [];
+      querySnapshot.forEach((doc) => {
+        itemsArr.push({ ...doc.data(), id: doc.id });
+      });
+      setItems(itemsArr);
+
+      // Read total from itemsArr
+      const calculateTotal = () => {
+        const totalPrice = itemsArr.reduce(
+          (sum, item) => sum + parseFloat(item.price),
+          0
+        );
+        setTotal(totalPrice);
+      };
+
+      calculateTotal();
+      return () => unsubscribe();
+    });
+  }, []);
 
   //Delete Items from database
+  const deleteItem = async (id) => {
+    await deleteDoc(doc(db, "items", id));
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
@@ -70,7 +105,10 @@ export default function Home() {
                   <span className="capitalize">{item.name}</span>
                   <span>${item.price}</span>
                 </div>
-                <button className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">
+                <button
+                  onClick={() => deleteItem(item.id)}
+                  className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16"
+                >
                   X
                 </button>
               </li>
